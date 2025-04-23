032a17b00b44db9d66c9e545a761710ffe058b50
Yoctopuce <dev@yoctopuce.com>
2025-04-23 08:38:09 +0200

    "New build based on revision 65971"

--- php8/yocto_groundspeed.php
+++ php8/yocto_groundspeed.php
@@ -53,7 +53,7 @@
  * YGroundSpeed Class: ground speed sensor control interface, available for instance in the Yocto-GPS-V2
  *
  * The YGroundSpeed class allows you to read and configure Yoctopuce ground speed sensors.
- * It inherits from YSensor class the core functions to read measurements,
+ * It inherits from YSensor class the core functions to read measures,
  * to register callback functions, and to access the autonomous datalogger.
  */
 class YGroundSpeed extends YSensor
--- php8/yocto_tvoc.php
+++ php8/yocto_tvoc.php
@@ -54,7 +54,7 @@
  * the Yocto-VOC-V3
  *
  * The YTvoc class allows you to read and configure Yoctopuce Total Volatile Organic Compound sensors.
- * It inherits from YSensor class the core functions to read measurements,
+ * It inherits from YSensor class the core functions to read measures,
  * to register callback functions, and to access the autonomous datalogger.
  */
 class YTvoc extends YSensor
